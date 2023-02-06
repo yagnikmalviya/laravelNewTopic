baa7543eecd02345c96955cb61671bab110d9777
yagnikmalviya <95216271+yagnikmalviya@users.noreply.github.com>
2023-02-06 19:28:08 +0530

    some issue solved

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\AuthController;
-use App\Http\Controllers\Admin\LayoutController;
-use App\Http\Controllers\Admin\ExaleGenerateController;
-use App\Http\Controllers\Front\GeneralController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,36 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('login', [AuthController::class, 'Login'])->name('login');
-Route::get('logout', [AuthController::class, 'Logout'])->name('logout');
-Route::post('loginInsert', [AuthController::class, 'LoginInsert'])->name('loginInsert');
 
-Route::group(['middleware' => 'Login'], function ()
-{
-    // xlsx import and export
-    Route::get('/xlsxView',[ExaleGenerateController::class,'XlsxeView'])->name('xlsxTable');
-    Route::post('/xlsxImport',[ExaleGenerateController::class,'XlsxImport'])->name('xlsxImport');
-    Route::get('/xlsxExport',[ExaleGenerateController::class,'XlsxExport'])->name('xlsxExport');
-    Route::get('/pdfExport',[ExaleGenerateController::class,'pdfExport'])->name('pdfExport');
+Route::prefix('admin')->group(base_path('routes/Admin.php'));
 
-    // Create Custom Blade Directive
-    Route::get('/directive',[GeneralController::class,'Directive']);
-
-    //Component
-    Route::get('components', [GeneralController::class, 'Component'])->name('component');
-
-    // Change Language
-    Route::get('/language',[GeneralController::class,'language'])->name('language');
-    Route::get('/languageChange',[GeneralController::class,'languageChange'])->name('languageChange');
-
-    Route::get('facebook', [GeneralController::class, 'redirectToFacebook']);
-    Route::get('facebook/callback', [GeneralController::class, 'handleFacebookCallback']);
-
-    Route::get('google', [GeneralController::class, 'redirectToGoogle']);
-    Route::get('google/callback', [GeneralController::class, 'handleGoogleCallback']);
-
-    Route::get('/', [LayoutController::class, 'dashboard'])->name('dashboard');
-});
 
 
 
